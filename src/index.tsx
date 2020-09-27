5956e2ed977e132c511ff2127984da6aec0f8a20
Vladyslav <spone379@gmail.com>
2020-09-27 15:13:24 +0300

    code refactor

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.scss';
-import App from './App';
 import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
+
+import './scss/index.scss';
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store} >
