0aad316288ebd18a82ee0d01402fda3f850e26c6
Vladyslav <spone379@gmail.com>
2020-09-27 15:14:03 +0300

    Update NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 
 import { ROUTES } from '../../config/constants';
 
+import './NotFound.scss';
+
 const NotFound = () => {
   return (
-    <div>
+    <div className="not-found">
       OOPS, Page Not Found!
       <Link to={ROUTES.main}>Go to the main page</Link>
     </div>
