32faf79dd2f55d100d8b0d27305da0cc0e91651f
Vladyslav <spone379@gmail.com>
2021-07-28 17:09:31 +0300

    add prop spinnerSize to GlobalPreloader

--- src/components/GlobalPreloader/GlobalPreloader.tsx
+++ src/components/GlobalPreloader/GlobalPreloader.tsx
@@ -1,12 +1,13 @@
 import './GlobalPreloader.scss';
+
 import Spinner from '../Spinner/Spinner';
 
-const GlobalPreloader = () => (
-  <div className="global-preloader">
+const GlobalPreloader = ({ spinnerSize = '150px' }: { spinnerSize?: string }) => {
+  return <div className="global-preloader">
     <div className="global-preloader__wrap">
-      <Spinner size="150px" />
+      <Spinner size={spinnerSize} />
     </div>
   </div>
-);
+};
 
 export default GlobalPreloader;
